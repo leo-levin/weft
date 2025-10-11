93d02df2733ffa1fa20139d3b3f0a4bc0b907745
leo frankel <llfrankel@uchicago.edu>
2025-10-10 20:21:44 -0500

    building parser

--- wrust/src/ast.rs
+++ wrust/src/ast.rs
@@ -1,85 +1,103 @@
 use std::collections::HashMap;
 
+#[derive(Clone)]
 pub struct BinaryExpr {
     pub op: String,
     pub left: Box<ASTNode>,
     pub right: Box<ASTNode>,
 }
 
+#[derive(Clone)]
 pub struct UnaryExpr {
     pub op: String,
     pub expr: Box<ASTNode>,
 }
 
+#[derive(Clone)]
 pub struct CallExpr {
     pub name: Box<ASTNode>,
     pub args: Vec<ASTNode>,
 }
 
+#[derive(Clone)]
 pub struct VarExpr {
     pub name: String,
 }
 
+#[derive(Clone)]
 pub struct NumExpr {
     pub v: f64,
 }
 
+#[derive(Clone)]
 pub struct StrExpr {
     pub v: String,
 }
 
+#[derive(Clone)]
 pub struct MeExpr {
     pub field: String,
 }
 
 // Tuple: (expr1, expr2, ...)
+#[derive(Clone)]
 pub struct TupleExpr {
     pub items: Vec<ASTNode>,
 }
 
+#[derive(Clone)]
 pub struct IndexExpr {
     pub base: Box<ASTNode>,
     pub index: Box<ASTNode>,
 }
 
+#[derive(Clone)]
 pub struct StrandAccessExpr {
     pub base: Box<ASTNode>,
     pub out: Box<ASTNode>,
 }
 
+#[derive(Clone)]
 pub struct StrandRemapExpr {
     pub base: Box<ASTNode>,
     pub strand: String,
     pub mappings: Vec<AxisMapping>,
 }
+
+#[derive(Clone)]
 pub struct AxisMapping {
     pub axis: String,
     pub expr: Box<ASTNode>,
 }
 
+#[derive(Clone)]
 pub struct IfExpr {
     pub condition: Box<ASTNode>,
     pub then_expr: Box<ASTNode>,
     pub else_expr: Box<ASTNode>,
 }
 
+#[derive(Clone)]
 pub struct AssignmentExpr {
     pub name: String,
     pub op: String,
     pub expr: Box<ASTNode>,
 }
 
+#[derive(Clone)]
 pub struct NamedArg {
     pub name: String,
     pub value: Box<ASTNode>,
 }
 
+#[derive(Clone)]
 pub struct OutputStatement {
     pub args: Vec<ASTNode>,
     pub named_args: HashMap<String, ASTNode>,
     pub positional_args: Vec<ASTNode>,
 }
 
+#[derive(Clone)]
 pub struct SpindleDef {
     pub name: String,
     pub inputs: Vec<String>,
@@ -87,16 +105,19 @@ pub struct SpindleDef {
     pub body: Box<ASTNode>,
 }
 
+#[derive(Clone)]
 pub struct InstanceBindExpr {
     pub name: String,
     pub outputs: Vec<String>,
     pub expr: Box<ASTNode>,
 }
 
+#[derive(Clone)]
 pub struct Program {
     pub statements: Vec<ASTNode>,
 }
 
+#[derive(Clone)]
 pub enum ASTNode {
     // Expressions
     Binary(BinaryExpr),
--- wrust/src/parser.rs
+++ wrust/src/parser.rs
@@ -22,7 +22,7 @@ fn build_program(pair: Pair<Rule>) -> Program {
                 let stmt_pair = inner_pair.into_inner().next().unwrap();
                 statements.push(build_statement(stmt_pair));
             }
-            Rule::EOI => {} // End of input, ignore
+            Rule::EOI => {}
             _ => unreachable!(),
         }
     }
@@ -89,16 +89,17 @@ fn build_logical_expr(pair: Pair<Rule>) -> ASTNode {
 
 fn build_spindle_def(pair: Pair<Rule>) -> ASTNode {
     // TODO(human): Implement spindle definition parsing
+    // Grammar: "spindle" ~ ident ~ "(" ~ ident_list ~ ")" ~ "::" ~ output_spec ~ block
+    // Children (no literals): ident, ident_list, output_spec, block
+    // Hint: Use build_ident_list() and build_output_spec() helpers
+    // Hint: build_block() returns a different type - check the AST!
     todo!("Implement spindle_def parsing")
 }
 
 fn build_env_assignment(pair: Pair<Rule>) -> ASTNode {
     let mut inner = pair.into_inner();
-    inner.next();
-    inner.next();
+    // Only 2 children: ident and expr (literals don't create children)
     let ident = inner.next().unwrap().as_str().to_string();
-    inner.next();
-    inner.next();
     let expr = Box::new(build_expr(inner.next().unwrap()));
 
     ASTNode::Assignment(AssignmentExpr {
@@ -120,17 +121,79 @@ fn build_instance_binding(pair: Pair<Rule>) -> ASTNode {
 }
 
 fn build_multi_spindle_call(pair: Pair<Rule>) -> ASTNode {
-    todo!("Implement compute_stmt parsing")
+    let mut inner = pair.into_inner();
+
+    let func_name = inner.next().unwrap().as_str().to_string();
+    let multiplier = inner.next().unwrap().as_str().parse::<usize>().unwrap();
+    let args_list = inner.next().unwrap();
+    let name = inner.next().unwrap().as_str().to_string();
+    let outputs = build_output_spec(inner.next().unwrap());
+
+    let func_var = Box::new(ASTNode::Var(VarExpr { name: func_name }));
+
+    let mut args_slots: Vec<Vec<ASTNode>> = Vec::new();
+    for bundle_or_expr_pair in args_list.into_inner() {
+        let arg_inner = bundle_or_expr_pair.into_inner().next().unwrap();
+        match arg_inner.as_rule() {
+            Rule::expr_list => {
+                let items = build_expr_list(arg_inner);
+                if items.len() != multiplier {
+                    panic!(
+                        "Bundle has {} items, but multi is {}",
+                        items.len(),
+                        multiplier
+                    );
+                }
+                args_slots.push(items);
+            }
+            Rule::expr => {
+                let single = build_expr(arg_inner);
+                arg_slots.push((0..multiplier).map(|_| single.clone()).collect());
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    let mut calls = Vec::new();
+    for i in 0..multiplier {
+        let call_args: Vec<ASTNode> = arg_slots.iter().map(|slot| slot[i].clone()).collect();
+        calls.push(ASTNode::Call(CallExpr {
+            name: func_var.clone(),
+            args: call_args,
+        }));
+    }
+
+    ASTNode::InstanceBinding(InstanceBindExpr {
+        name,
+        outputs,
+        expr: Box::new(ASTNode::Tuple(TupleExpr { items: calls })),
+    })
 }
+
 fn build_spindle_call(pair: Pair<Rule>) -> ASTNode {
-    todo!("Implement compute_stmt parsing")
+    let mut inner = pair.into_inner();
+    let func_name = inner.next().unwrap().as_str().to_string();
+    let func_var = Box::new(ASTNode::Var(VarExpr { name: func_name }));
+    let args = build_expr_list(inner.next().unwrap());
+    let expr = Box::new(ASTNode::Call(CallExpr {
+        name: func_var,
+        args,
+    }));
+
+    let name = inner.next().unwrap().as_str().to_string();
+    let outputs = build_output_spec(inner.next().unwrap());
+
+    ASTNode::InstanceBinding(InstanceBindExpr {
+        name,
+        outputs,
+        expr,
+    })
 }
 
 fn build_direct_bind(pair: Pair<Rule>) -> ASTNode {
     let mut inner = pair.into_inner();
     let name = inner.next().unwrap().as_str().to_string();
     let outputs = build_output_spec(inner.next().unwrap());
-    inner.next();
     let expr = Box::new(build_expr(inner.next().unwrap()));
 
     ASTNode::InstanceBinding(InstanceBindExpr {
@@ -142,7 +205,7 @@ fn build_direct_bind(pair: Pair<Rule>) -> ASTNode {
 
 fn build_output_spec(pair: Pair<Rule>) -> Vec<String> {
     let mut inner = pair.into_inner();
-    inner.next();
+
     let id_list = inner.next().unwrap();
 
     id_list
@@ -152,25 +215,218 @@ fn build_output_spec(pair: Pair<Rule>) -> Vec<String> {
 }
 
 fn build_render_stmt(pair: Pair<Rule>) -> ASTNode {
+    // TODO(human): Implement output statement parsing
+    // Grammar: "render" ~ "(" ~ stmt_arg_list ~ ")"
+    // Child: stmt_arg_list
+    // Use build_stmt_arg_list() helper
+    // Return RenderStmt (check OutputStatement in AST)
     todo!("Implement render_stmt parsing")
 }
 
 fn build_play_stmt(pair: Pair<Rule>) -> ASTNode {
+    // TODO(human): Same pattern as render_stmt
     todo!("Implement play_stmt parsing")
 }
 
 fn build_compute_stmt(pair: Pair<Rule>) -> ASTNode {
+    // TODO(human): Same pattern as render_stmt
     todo!("Implement compute_stmt parsing")
 }
 
 fn build_pragma(pair: Pair<Rule>) -> ASTNode {
+    let mut inner = pair.into_inner();
+
+    // TODO(human): Simple pragma parsing
+    // Grammar: "#" ~ ident ~ pragma_body
+    // Children: ident (type), pragma_body
+    // pragma_body.as_str() gives the full text
+    // Note: Pragma validation happens at runtime, not parse time!
     todo!("Implement pragma parsing")
 }
 
 fn build_if_expr(pair: Pair<Rule>) -> ASTNode {
-    todo!("Implement if_expr parsing")
+    let mut inner = pair.into_inner();
+    let condition = Box::new(build_expr(inner.next().unwrap()));
+    let then_expr = Box::new(build_expr(inner.next().unwrap()));
+    let else_expr = Box::new(build_expr(inner.next().unwrap()));
+
+    ASTNode::If(IfExpr {
+        condition,
+        then_expr,
+        else_expr,
+    })
 }
 
 fn build_comparison_expr(pair: Pair<Rule>) -> ASTNode {
-    todo!("Implement comparison_expr parsing")
+    let mut inner = pair.into_inner();
+
+    let left = build_arith_expr(inner.next().unwrap());
+
+    if let Some(op_pair) = inner.next() {
+        let op = op_pair.as_str().to_string();
+        let right = build_arith_expr(inner.next().unwrap());
+
+        ASTNode::Binary(BinaryExpr {
+            op,
+            left: Box::new(left),
+            right: Box::new(right),
+        })
+    } else {
+        left
+    }
+}
+
+fn build_arith_expr(pair: Pair<Rule>) -> ASTNode {
+    let mut inner = pair.into_inner();
+
+    let mut left = build_term(inner.next().unwrap());
+
+    while let Some(op_pair) = inner.next() {
+        let op = op_pair.as_str().to_string();
+        let right = build_term(inner.next().unwrap());
+
+        left = ASTNode::Binary(BinaryExpr {
+            op,
+            left: Box::new(left),
+            right: Box::new(right),
+        });
+    }
+
+    left
+}
+
+fn build_term(pair: Pair<Rule>) -> ASTNode {
+    let mut inner = pair.into_inner();
+
+    let mut left = build_factor(inner.next().unwrap());
+
+    while let Some(op_pair) = inner.next() {
+        let op = op_pair.as_str().to_string();
+        let right = build_factor(inner.next().unwrap());
+
+        left = ASTNode::Binary(BinaryExpr {
+            op,
+            left: Box::new(left),
+            right: Box::new(right),
+        });
+    }
+    left
+}
+
+fn build_factor(pair: Pair<Rule>) -> ASTNode {
+    let inner = pair.into_inner().next().unwrap();
+    build_power(inner)
+}
+
+fn build_power(pair: Pair<Rule>) -> ASTNode {
+    let mut inner = pair.into_inner();
+
+    let left = build_unary(inner.next().unwrap());
+
+    if let Some(_caret) = inner.next() {
+        let right = build_power(inner.next().unwrap());
+
+        ASTNode::Binary(BinaryExpr {
+            op: "^".to_string(),
+            left: Box::new(left),
+            right: Box::new(right),
+        })
+    } else {
+        left
+    }
+}
+
+fn build_unary(pair: Pair<Rule>) -> ASTNode {
+    let inner = pair.into_inner().next().unwrap();
+
+    match inner.as_rule() {
+        Rule::unary => {
+            // Recursive unary (-, not)
+            let mut unary_inner = inner.into_inner();
+            let op_pair = unary_inner.next().unwrap();
+            let op = if op_pair.as_str() == "-" {
+                "-".to_string()
+            } else {
+                "NOT".to_string()
+            };
+            let expr = build_unary(unary_inner.next().unwrap());
+
+            ASTNode::Unary(UnaryExpr {
+                op,
+                expr: Box::new(expr),
+            })
+        }
+        Rule::primary => build_primary(inner),
+        _ => unreachable!(),
+    }
+}
+
+fn build_primary(pair: Pair<Rule>) -> ASTNode {
+    let inner = pair.into_inner().next().unwrap();
+
+    match inner.as_rule() {
+        Rule::number => {
+            let value = inner.as_str().parse::<f64>().unwrap();
+            ASTNode::Num(NumExpr { v: value })
+        }
+        Rule::string => ASTNode::Str(StrExpr {
+            v: inner.as_str().to_string(),
+        }),
+        Rule::ident => ASTNode::Var(VarExpr {
+            name: inner.as_str().to_string(),
+        }),
+        Rule::expr => build_expr(inner),
+        Rule::expr_list => {
+            let items = build_expr_list(inner);
+            ASTNode::Tuple(TupleExpr { items: items })
+        }
+        _ => todo!("Implement other primary expressions: {:?}", inner.as_rule()),
+    }
+}
+
+// ============================================================================
+// HELPER FUNCTIONS
+// ============================================================================
+
+fn build_ident_list(pair: Pair<Rule>) -> Vec<String> {
+    pair.into_inner()
+        .map(|ident_pair| ident_pair.as_str().to_string())
+        .collect()
+}
+
+fn build_expr_list(pair: Pair<Rule>) -> Vec<ASTNode> {
+    pair.into_inner()
+        .map(|expr_pair| build_expr(expr_pair))
+        .collect()
+}
+
+fn build_stmt_arg_list(pair: Pair<Rule>) -> Vec<ASTNode> {
+    // TODO(human): Parse statement arguments (named and positional)
+    // Grammar: (stmt_arg ~ ("," ~ stmt_arg)*)?
+    // Each stmt_arg is either: ident ~ ":" ~ expr (named) OR expr (positional)
+    // Hint: Check stmt_arg.as_rule() to see which variant
+    // Named args become NamedArg nodes, positional are just expressions
+    todo!("Implement stmt_arg_list parsing")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_simple_assignment() {
+        let source = "strand<x> = 5";
+        let result = parse(source);
+
+        match result {
+            Ok(program) => {
+                println!("✓ Parsed successfully!");
+                println!("Program has {} statements", program.statements.len());
+            }
+            Err(e) => {
+                println!("✗ Parse error: {}", e);
+                panic!("Failed to parse");
+            }
+        }
+    }
 }
